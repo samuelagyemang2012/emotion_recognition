d357992b23efc3570fc42acefbdd14253d4efe58
samuelagyemang2012 <khermz2012@gmail.com>
2021-06-04 15:04:34 +0800

    n/a changes

--- tools/face_cropper2.py
+++ tools/face_cropper2.py
@@ -4,28 +4,33 @@ from matplotlib import pyplot as plt
 
 backends = ['retinaface']
 
-INPUT_PATH = "G:/datasets/Alice/Data/African/original/happy_edited/"
-OUTPUT_PATH = "G:/datasets/Alice/Data/African/cropped_faces/happy_cropped_faces/"
-EMOTION = "happy"
-ETH = "afr"
+INPUT_PATH = "G:/datasets/Alice/Data/Chinese/original/angry/"
+OUTPUT_PATH = "G:/datasets/Alice/Data/Chinese/cropped_faces/angry_cropped_faces/"
+EMOTION = "angry"
+ETH = "chn"
 CHANNELS = 3
-
 INPUT_SHAPE = (100, 100)
 
+# Read all files in the folder
 files = os.listdir(INPUT_PATH)
 
 for i, f in enumerate(files):
 
     img_path = INPUT_PATH + f
-    img = functions.preprocess_face(img_path, target_size=INPUT_SHAPE, detector_backend=backends[0])
-    if img.size != 0:
+
+    # Detect faces in image
+    try:
+        img = functions.preprocess_face(img_path, target_size=INPUT_SHAPE, detector_backend=backends[0])
+
         img = img.reshape(INPUT_SHAPE[0], INPUT_SHAPE[0], CHANNELS)
-        dest = OUTPUT_PATH+ETH + "_" + EMOTION + "_" + str(i)+".jpg"
+
+        dest = OUTPUT_PATH + ETH + "_" + EMOTION + "_" + str(i) + ".jpg"
         # dest = "../test/" + ETH + "_" + EMOTION + "_" + str(i) + ".jpg"
+        # Save the image file
         plt.imsave(dest, img[:, :, ::-1], format="jpg")
-        c = i+1
+        c = i + 1
         print("Processed: " + str(c) + " images")
-    else:
-        print("No face detected!")
+    except:
+        print("No face detected! -> "+files[i])
 
 print("done")
